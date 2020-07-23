5ce2ca08016be528cdcf4070a528099035a30acb
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-07-22 17:30:34 -0700

    update

--- packet-reader.py
+++ packet-reader.py
@@ -1,9 +1,28 @@
-byteFile = open('byte-stream.txt', 'rb')
-byte = byteFile.readline()
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+byteFile = open('byte-stream.txt', 'r')
+hexStream = byteFile.readline()
 
-while byte:
-    print(byte)
-    byte = byteFile.readline()
+# print(hexStream)
+
+ethernetFrame = hexStream[0:28] # Ethernet frame is a 14-byte header: two 6-byte addresses and a 2-byte type field
+print(bcolors.WARNING + ethernetFrame[0:12] + bcolors.ENDC + bcolors.OKGREEN + ethernetFrame[12:24] + bcolors.ENDC + bcolors.HEADER + ethernetFrame[24:28] + bcolors.ENDC)
+
+destinationEthernet = ethernetFrame[0:12] # Yellow
+print(destinationEthernet)
+
+sourceEthernet = ethernetFrame[12:24] # Green 
+print(sourceEthernet)
+
+typeFieldEthernet = ethernetFrame[24:28] # Purple
+print(typeFieldEthernet)
 
 byteFile.close()
